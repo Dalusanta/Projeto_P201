e662a21ae543370bb984ff64e3795f85f792c196
Matheus-P23 <matheusativado12@gmail.com>
2025-11-25 22:48:36 -0300

    Update TarefaPrioridade.java

--- TarefaPrioridade.java
+++ TarefaPrioridade.java
@@ -1,10 +1,22 @@
 public class TarefaPrioridade extends Tarefa {
-    int prioridade;
     
-    public TarefaPrioridade(String titulo, String descricao, int prioridade) {
+    private int prioridade; 
+    
+    public TarefaPrioridade(String titulo, String descricao, int prioridade) throws PrioridadeException {
         super(titulo, descricao); 
-        this.prioridade = prioridade;
         this.tipo = true; 
+        setPrioridade(prioridade); 
+    }
+
+    public void setPrioridade(int prioridade) throws PrioridadeException {
+        if (prioridade < 1 || prioridade > 5) {
+            throw new PrioridadeException("A prioridade deve ser um número entre 1 e 5.");
+        }
+        this.prioridade = prioridade;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
     }
 
     @Override
